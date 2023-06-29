510458f2ee51d24e023f9828ee282218baf93b90
Noman02 <noman.mahib001@gmail.com>
2023-06-29 20:14:10 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h3 className="heading">app component</h3>
+      <h4 className="title">I have started work again after so long time.</h4>
+    </div>
+  );
+};
+
+export default App;
